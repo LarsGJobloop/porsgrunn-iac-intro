30b4852c1d45b0647ed678ac1ca4f20a23aba875
zabronax <zabronax@gmail.com>
2025-06-27 12:14:36 +0200

    define development environment

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "An backend project covering several advanced concepts";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      withSystem = nixpkgs.lib.genAttrs [
+        "x86_64-linux" # TODO! NOT Verified
+        "x86_64-darwin" # TODO! NOT Verified
+        "aarch64-linux" # TODO! NOT Verified
+        "aarch64-darwin" # Verified
+      ];
+      withPkgs =
+        callback:
+        withSystem (
+          system:
+          callback (
+            import nixpkgs {
+              inherit system;
+              config.allowUnfree = true; # Terraform is unfree software
+            }
+          )
+        );
+    in
+    {
+      devShells = withPkgs (pkgs: {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.terraform # IaC language
+          ];
+
+          env = {
+            DOTNET_ROOT = builtins.toString pkgs.dotnetCorePackages.sdk_9_0;
+          };
+        };
+      });
+
+      formatter = withPkgs (pkgs: pkgs.nixfmt-rfc-style);
+    };
+}
