02efb6e88fefb5e9716406a88eb2154528724780
zabronax <zabronax@gmail.com>
2025-06-27 12:40:25 +0200

    setup repository

--- main.tf
+++ main.tf
@@ -10,3 +10,13 @@ terraform {
 provider "github" {
   token = "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"
 }
+
+# What exists
+resource "github_repository" "this_repository" {
+  name = "porsgrunn-iac-intro"
+  visibility = "private"
+}
+
+output "repository" {
+  value = github_repository.this_repository.html_url
+}
